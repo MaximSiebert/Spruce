788cc6ea5fdb5c05713e5c2a24bdd90747d8c44f
Garry Ing <garry@format.com>
2018-02-23 15:44:35 -0500

    menu spec

--- spec/frontend_spec.rb
+++ spec/frontend_spec.rb
@@ -3,9 +3,14 @@ require 'spec_helper'
 describe 'Spruce theme frontend', theme: 'spruce', type: :feature, shared_session: true do
   # it_behaves_like 'has logo support', image_max_height: 200, selector: '.logo'
   it_behaves_like 'page with social links', selector: '.social-link'
-  # it_behaves_like 'has menu', dropdown_categories: false,
-  #                 menu_item_selector: 'nav.menu .item',
-  #                 category_item_selector: 'nav.menu .category'
+  it_behaves_like 'has menu', dropdown_categories: true, open_mode: :click,
+                  menu_item_selector: 'nav.menu .item',
+                  category_item_selector: 'nav.menu .category',
+                  wait_callback: proc {
+                    if page.has_css?('.mobile-menu-toggle button')
+                      find('.mobile-menu-toggle button').click
+                    end
+                  }
   it_behaves_like 'has gallery support', lazy_load: true
   it_behaves_like 'has listing support', lazy_load: true,
                   title_selector: '.title-element',
